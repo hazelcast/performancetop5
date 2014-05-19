00d4a71961fbe880eb281276e0b0f9e8e7ea4b37
Peter Veentjer <alarmnummer@gmail.com>
2014-05-19 16:57:50 +0300

    Minor shellscript fix

--- item2/stabilizer/localbenchmark.sh
+++ item2/stabilizer/localbenchmark.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
+ITERATIONS=5
+
 echo ===================================== DATA SERIALIZABLE ======================================
 
 cd workdir-dataserializable
 
-for i in {1..5}
+for i in {1..$ITERATIONS}
 do
     ./static-run.sh
 done
+
+cd ..
 
 echo ===================================== IDENTIFIED SERIALIZABLE ======================================
 
 cd workdir-identifieddataserializable
 
-for i in {1..5}
+for i in {1..$ITERATIONS}
 do
     ./static-run.sh
 done
+
+cd ..
 
 echo ===================================== SERIALIZABLE ======================================
 
-cd workdir-identifieddataserializable
+cd workdir-serializable
 
-for i in {1..5}
+for i in {1..$ITERATIONS}
 do
     ./static-run.sh
 done
+
+cd ..
